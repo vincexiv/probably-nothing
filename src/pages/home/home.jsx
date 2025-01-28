47f056d9abd0c8b77f424a7da4c06cebcda0cf94
Vincent <makokhavomondi@gmail.com>
2025-01-28 18:00:08 +0300

    Some initial work

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home(){
+    return (
+        <div id="page-home" className="page">
+            <div className="page-container">
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
