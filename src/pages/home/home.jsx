5360646fc3ee59e7c4c3b7edd0272406b45d4207
Vincent <makokhavomondi@gmail.com>
2025-01-28 18:14:21 +0300

    Use mediaspaces app

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import M from 'mediaspace'
+import { useRef } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function Home(){
+    const ref = useRef()
+    const [ m, setM ] = useState(null)
+
+    useEffect(() => {
+        const m = new M(ref.current, {apiKey: 'yLqndIlkE0H6gAfIlZzzEms39_3_-95z'})
+        setM(m)
+    }, [])
+
     return (
         <div id="page-home" className="page">
             <div className="page-container">
+                <div className="photos" ref={ref}>
 
+                </div>
             </div>
         </div>
     )
